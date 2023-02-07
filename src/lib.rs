c014b9d3cdb6ad593e14bb7eb0de351bc3e001ff
Uzay-G <uzgirit@gmail.com>
2023-02-07 01:36:23 -0500

    wrestle with lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,56 @@
-use std::ops::{Add, Mul, Div, AddAssign};
+use std::ops::{Div, Mul, Add, Sub, AddAssign};
+use crate::ops::*; 
+use num_traits::Num;
+
+mod ops;
 
 // we have our tensor type
-struct Tensor<T> {
+struct Tensor<'a, T> {
     data: Vec<T>,
+    grad: Vec<T>,
     shape: Vec<usize>,
     stride: Vec<usize>,
+    op: Option<TensOp>,
+    l_parent: Option<&'a Tensor<'a, T>>,
+    r_parent: Option<&'a Tensor<'a, T>>
 }
 
+impl<'a, T: Num + Copy + Default> Tensor<'a, T> {
+    fn new(data: Vec<T>, shape: &Vec<usize>) -> Tensor<'a, T> {
+        let mut stride = vec![1];
+        for i in 0..shape.len() - 1 {
+            stride.push(stride[i] * shape[shape.len() - i - 1]);
+        }
+        stride.reverse();
+        let len = data.len();
+        Tensor {
+            data: data,
+            shape: shape.clone(),
+            stride: stride,
+            grad: vec![Default::default(); len],
+            op: None,
+            l_parent: None,
+            r_parent: None
+        }
+    }
+    fn new_parented(data: Vec<T>, shape: &Vec<usize>, op: TensOp, l_parent: Option<&'a Tensor<'a, T>>, r_parent: Option<&'a Tensor<'a, T>> ) -> Tensor<'a, T> {
+        let mut stride = vec![1];
+        for i in 0..shape.len() - 1 {
+            stride.push(stride[i] * shape[shape.len() - i - 1]);
+        }
+        stride.reverse();
+        let len = data.len();
+        Tensor {
+            data: data,
+            shape: shape.clone(),
+            stride: stride,
+            grad: vec![Default::default(); len],
+            op: Some(op),
+            l_parent: l_parent,
+            r_parent: r_parent
+        }
+    }
+}
 // rust macros are amazing
 // now let's actually implement broadcasting properly
 macro_rules! broadcast {
@@ -14,10 +58,11 @@ macro_rules! broadcast {
         // I've been thinking about how to implement broadcasting in a nice abstract way, that generalizes
         // across binary operators
         // rust is :heart:
-        impl<T: Default + $b_trait + $b_trait<Output = T> + Copy> $b_trait for Tensor<T> {
-            type Output = Tensor<T>;
-            fn $fn_name(self, rhs: Tensor<T>) -> Tensor<T> {
+        impl<'a, T: Num + Copy + Default + AddAssign>$b_trait for Tensor<'a, T> {
+            type Output = Tensor<'a, T>;
+            fn $fn_name(self, rhs: Tensor<T>) -> Tensor<'a, T> {
                 let mut res: Vec<T> = vec![];
+                let mut grad: Vec<T> = vec![];
                 let (smallest, largest) = if self.data.len() < rhs.data.len() {
                     (self, rhs)
                 } else {
@@ -26,27 +71,23 @@ macro_rules! broadcast {
                 //let mut i = smallest.shape.len() - 1;
                 //while smallest.shape[i] == largest.shape[i] && i >= 1 { i -= 1; }
                 let stride = smallest.data.len();
-                println!("stride: {}", stride);
-                println!("smallest: {:?}", smallest.data.len());
                 for i in 0..largest.data.len() {
                     res.push(largest.data[i].$fn_name(smallest.data[i % stride]));
                 }
-                return init_tensor(res, &largest.shape);
+                return Tensor::new(res, &largest.shape);
             }
         }
 
-        impl<T: $b_trait + $b_trait<Output = T> + Copy> $b_trait<T> for Tensor<T> {
-            type Output = Tensor<T>;
-            fn $fn_name(self, rhs: T) -> Tensor<T> {
+        impl<'a, T: Num + Copy + Default + AddAssign> $b_trait<T> for Tensor<'a, T> {
+            type Output = Tensor<'a, T>;
+            fn $fn_name(self, rhs: T) -> Tensor<'a, T> {
                 let mut res: Vec<T> = vec![];
                 for i in 0..self.data.len() {
                     res.push(self.data[i].$fn_name(rhs));
                 }
-                return init_tensor(res, &self.shape);
+                return Tensor::new(res, &self.shape);
             }
         }
-
-
     }
 }
 
@@ -57,11 +98,11 @@ macro_rules! broadcast {
 // can't easily represent numerical types, should check out num
 macro_rules! implement_reduce {
     ($fn_name:ident, $dim_fn:ident, $reduc_output:ident) => {
-        impl<T: Div + Copy + Add + AddAssign + Default> Tensor<T> where f64: From<T>, T: Div<Output = T> {
+        impl<'a, T: Num + Copy + Default + AddAssign> Tensor<'a, T> where f64: From<T>, T: Div<Output = T> {
             fn $fn_name(self) -> $reduc_output {
                 return $fn_name(&self.data);
             }
-            fn $dim_fn(self, dim: usize) -> Tensor<$reduc_output> {
+            fn $dim_fn(self, dim: usize) -> Tensor<'a, $reduc_output> {
                 if dim >= self.shape.len() { panic!("Dimension too large. Tensor dims: {}, called dim: {}", self.shape.len(), dim); }
                 // TODO: implement negative index later
                 let mut res = vec![];
@@ -82,7 +123,7 @@ macro_rules! implement_reduce {
                 let shape = if dim == 0 {
                     Vec::from([1])
                 } else { self.shape[0..dim].to_vec() };
-                return init_tensor(res, &shape);
+                return Tensor::new(res, &shape);
             }
         }
     }
@@ -92,41 +133,11 @@ macro_rules! implement_reduce {
 broadcast!(Add, add);
 broadcast!(Mul, mul);
 broadcast!(Div, div);
+broadcast!(Sub, sub);
 
-fn sum<T: Copy + Default + Add + AddAssign>(slice: &[T]) -> T {
-    let mut sum: T = Default::default();
-    for i in slice.iter() {
-        sum += i.clone() // is this bad practice?
-    }
-    return sum;
-}
-
-// think about efficiency
-fn mean<T: Copy + Default + Add + Div + Div<Output = T> + AddAssign + Into<f64>>(slice: &[T]) -> f64 {
-
-    let mut sum: T = Default::default();
-    for i in slice.iter() {
-        sum += i.clone() // is this bad practice?
-    }
-    let sum2: f64 = sum.into();
-    return sum2 / slice.len() as f64;
-}
 
 implement_reduce!{sum, sum_dim, T}
 implement_reduce!{mean, mean_dim, f64}
-
-fn init_tensor<T>(data: Vec<T>, shape: &Vec<usize>) -> Tensor<T> {
-    let mut stride = vec![1];
-    for i in 0..shape.len() - 1 {
-        stride.push(stride[i] * shape[shape.len() - i - 1]);
-    }
-    stride.reverse();
-    Tensor {
-        data: data,
-        shape: shape.clone(),
-        stride: stride,
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -136,7 +147,7 @@ mod tests {
     fn test_tensor_creation_2d() {
         let data = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape = vec![2, 3];
-        let tensor = init_tensor(data, &shape);
+        let tensor = Tensor::new(data, &shape);
         assert_eq!(tensor.data, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
         assert_eq!(tensor.shape, vec![2, 3]);
         assert_eq!(tensor.stride, vec![3, 1]);
@@ -146,7 +157,7 @@ mod tests {
     fn test_tensor_creation_3d() {
         let data = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
         let shape = vec![2, 2, 2];
-        let tensor = init_tensor(data, &shape);
+        let tensor = Tensor::new(data, &shape);
         assert_eq!(tensor.data, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
         assert_eq!(tensor.shape, vec![2, 2, 2]);
         assert_eq!(tensor.stride, vec![4, 2, 1]);
@@ -157,8 +168,8 @@ mod tests {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let data2 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape);
-        let tensor2 = init_tensor(data2, &shape);
+        let tensor1 = Tensor::new(data1, &shape);
+        let tensor2 = Tensor::new(data2, &shape);
         let tensor3 = tensor1 + tensor2;
         assert_eq!(tensor3.data, vec![2.0, 4.0, 6.0, 8.0, 10.0, 12.0]);
     }
@@ -167,7 +178,7 @@ mod tests {
     fn test_add_scalar() {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape);
+        let tensor1 = Tensor::new(data1, &shape);
         let tensor2 = tensor1 + 1.0;
         assert_eq!(tensor2.data, vec![2.0, 3.0, 4.0, 5.0, 6.0, 7.0]);
     }
@@ -177,8 +188,8 @@ mod tests {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let data2 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape);
-        let tensor2 = init_tensor(data2, &shape);
+        let tensor1 = Tensor::new(data1, &shape);
+        let tensor2 = Tensor::new(data2, &shape);
         let tensor3 = tensor1 * tensor2;
         assert_eq!(tensor3.data, vec![1.0, 4.0, 9.0, 16.0, 25.0, 36.0]);
     }
@@ -188,8 +199,8 @@ mod tests {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let data2 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape);
-        let tensor2 = init_tensor(data2, &shape);
+        let tensor1 = Tensor::new(data1, &shape);
+        let tensor2 = Tensor::new(data2, &shape);
         let tensor3 = tensor1 / tensor2;
         assert_eq!(tensor3.data, vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
     }
@@ -201,8 +212,8 @@ mod tests {
         let data2 = vec![1.0, 2.0];
         let shape1 = vec![2, 3, 2];
         let shape2 = vec![3];
-        let tensor1 = init_tensor(data1, &shape1);
-        let tensor2 = init_tensor(data2, &shape2);
+        let tensor1 = Tensor::new(data1, &shape1);
+        let tensor2 = Tensor::new(data2, &shape2);
         let tensor3 = tensor1 * tensor2;
         println!("{:?}", tensor3.data);
         assert_eq!(tensor3.data, vec![1.0, 4.0, 3.0, 8.0, 5.0, 12.0, 7.0, 16.0, 9.0, 20.0, 11.0, 24.0]);
@@ -215,8 +226,8 @@ mod tests {
         let data2 = vec![1.0, 2.0, 3.0];
         let shape1 = vec![2, 3];
         let shape2 = vec![3];
-        let tensor1 = init_tensor(data1, &shape1);
-        let tensor2 = init_tensor(data2, &shape2);
+        let tensor1 = Tensor::new(data1, &shape1);
+        let tensor2 = Tensor::new(data2, &shape2);
         let tensor3 = tensor1 * tensor2;
         assert_eq!(tensor3.data, vec![1.0, 4.0, 9.0, 4.0, 10.0, 18.0]);
     }
@@ -225,7 +236,7 @@ mod tests {
     fn test_tensor_sum() {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape1 = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape1);
+        let tensor1 = Tensor::new(data1, &shape1);
         let sum = tensor1.sum();
         assert_eq!(sum, 21.0); // ugh
 
@@ -235,7 +246,7 @@ mod tests {
     fn test_tensor_mean() {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape1 = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape1);
+        let tensor1 = Tensor::new(data1, &shape1);
         let mean = tensor1.mean();
         assert_eq!(mean, 3.5); // ugh
     }
@@ -244,7 +255,7 @@ mod tests {
     fn test_tensor_sum_dim() {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape1 = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape1);
+        let tensor1 = Tensor::new(data1, &shape1);
         let sum = tensor1.sum_dim(1);
         assert_eq!(sum.data, vec![6.0, 15.0]);
     }
@@ -253,7 +264,7 @@ mod tests {
     fn test_tensor_mean_dim() {
         let data1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape1 = vec![2, 3];
-        let tensor1 = init_tensor(data1, &shape1);
+        let tensor1 = Tensor::new(data1, &shape1);
         let sum = tensor1.mean_dim(1);
         assert_eq!(sum.data, vec![2.0, 5.0]);
     }
---
+++ src/ops.rs
@@ -0,0 +1,35 @@
+use std::ops::{AddAssign};
+use num_traits::Num;
+
+pub enum TensOp {
+    Add,
+    Mul,
+    Div,
+    Sub,
+}
+
+pub fn sum<T: Copy + Default + AddAssign>(slice: &[T]) -> T {
+    let mut sum: T = Default::default();
+    for i in slice.iter() {
+        sum += i.clone() // is this bad practice?
+    }
+    return sum;
+}
+
+// think about efficiency
+pub fn mean<T: Copy + Default + Num + AddAssign + Into<f64>>(slice: &[T]) -> f64 {
+
+    let mut sum: T = Default::default();
+    for i in slice.iter() {
+        sum += i.clone() // is this bad practice?
+    }
+    let sum2: f64 = sum.into();
+    return sum2 / slice.len() as f64;
+}
+
+pub fn add<T: Copy + Num>(x: T, y: T) -> T {
+    return x + y;
+}
+pub fn mul<T: Copy + Num>(x: T, y: T) -> T {
+    return x * y;
+}
